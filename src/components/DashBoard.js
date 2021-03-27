039ab3eff7f2746e67574b239479b5bf27b6e649
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-03-27 16:11:57 +0200

    update

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import GoogleMaps from './GoogleMaps';
+import React from "react";
+import GoogleMaps from "./GoogleMaps";
 
 
+const Dashboard = (props) => {
+  // API_KEY= AIzaSyA4WZVgyVFWRwW-JD1-qBojpP5sW9-5Rew
 
-const Dashboard = (props)=> {
+  const handleLogout = () => {
+    props.history.push("/login");
+  };
 
-    // API_KEY= AIzaSyA4WZVgyVFWRwW-JD1-qBojpP5sW9-5Rew
+  return (
+    <div className="dashboard">
+      <div className="navdash">
+        Welcome !<button onClick={handleLogout}>LogOut</button>
+      </div>
+      <div >
+        <GoogleMaps />
+      </div>
+      
+    </div>
+  );
+};
 
-    const handleLogout = () => {
-        props.history.push('/login')
-    }
-
-    return (
-        <div className='dashboard'>
-            Welcome !
-            <button onClick={handleLogout}>LogOut</button>
-            <div>
-                <GoogleMaps />
-            </div>
-        </div>
-    )
-}
-
-export default Dashboard
+export default Dashboard;
--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -1,52 +1,50 @@
-import React, {Component} from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} 
-from 'google-maps-react';
-
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import '../Css/GoogleMaps.css'
 
 export class MapContainer extends Component {
-    state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
-    };
-   
-    onMarkerClick = (props, marker, e) =>
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
       this.setState({
-        selectedPlace: props,
-        activeMarker: marker,
-        showingInfoWindow: true
+        showingInfoWindow: false,
+        activeMarker: null,
       });
-   
-    onMapClicked = (props) => {
-      if (this.state.showingInfoWindow) {
-        this.setState({
-          showingInfoWindow: false,
-          activeMarker: null
-        })
-      }
-    };
-   
-    render() {
-      return (
-        <Map google={this.props.google}
-            onClick={this.onMapClicked}>
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-   
+    }
+  };
+
+  render() {
+    return (
+      <div className='googlemaps'>
+        <Map google={this.props.google} onClick={this.onMapClicked}>
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
           <InfoWindow
             marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}>
-              <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-              </div>
+            visible={this.state.showingInfoWindow}
+          >
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
           </InfoWindow>
         </Map>
-      )
-    }
+      </div>
+    );
   }
+}
 
- 
- 
 export default GoogleApiWrapper({
-  apiKey: ('AIzaSyA4WZVgyVFWRwW-JD1-qBojpP5sW9-5Rew')
-})(MapContainer)
+  apiKey: "AIzaSyA4WZVgyVFWRwW-JD1-qBojpP5sW9-5Rew",
+})(MapContainer);
